3be994270479b970c08ff3b94f04356d97dbe863
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-10-18 00:08:08 +0000

    Update print() to use f-strings.

--- aes_block.py
+++ aes_block.py
@@ -4,7 +4,7 @@ from os import urandom
 
 # Pick a random 16-byte key using Python's crypto PRNG.
 k = urandom(16)
-print("k = {}".format(hexa(k)))
+print(f"k = {hexa(k)}")
 
 # Create AES-128 block cipher to encrypt a single block.
 cipher = Cipher(algorithms.AES(k), modes.ECB())
@@ -16,9 +16,9 @@ p = p.encode()
 
 # Encrypt plaintext p (to ciphertext c).
 c = aes_encrypt.update(p) + aes_encrypt.finalize()
-print("enc({}) = {}".format(hexa(p), hexa(c)))
+print(f"enc({hexa(p)}) = {hexa(c)}")
 
 # Decrypt ciphertext c to plaintext p.
 aes_decrypt = cipher.decryptor()
 p = aes_decrypt.update(c) + aes_decrypt.finalize()
-print("dec({}) = {}".format(hexa(c), hexa(p)))
+print(f"dec({hexa(c)}) = {hexa(p)}")
