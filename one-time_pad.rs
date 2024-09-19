177f2ecd84fd9b3eca4b98c5106549f42e25ec96
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-19 00:47:27 +0000

    Play with one-time pad.

---
+++ one-time_pad.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let plaintext: u8 = 0b0110_1101;
+    let key: u8 = 0b1011_0100;
+
+    let ciphertext = plaintext ^ key;
+    println!("C = P ⨁ K = {:b} ⨁ {:b} = {:b}", plaintext, key, ciphertext);
+
+    assert_eq!(ciphertext ^ key, plaintext);
+}
