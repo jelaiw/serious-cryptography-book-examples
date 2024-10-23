011468cc6bd47800c6632524158dc7ca3e776218
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-23 23:30:45 +0000

    Encrypt/decrypt a single block.

--- Chapter_4_Block_Ciphers/aes_block/src/main.rs
+++ Chapter_4_Block_Ciphers/aes_block/src/main.rs
@@ -1,4 +1,9 @@
 use rand::{rngs::OsRng, RngCore};
+use aes::Aes128;
+use aes::cipher::{
+    BlockCipher, BlockEncrypt, BlockDecrypt, KeyInit,
+    generic_array::GenericArray,
+};
 
 fn main() {
     let mut key = [0u8; 16];
@@ -6,6 +11,22 @@ fn main() {
     // https://stackoverflow.com/q/71048413
     // https://docs.rs/rand/latest/rand/trait.RngCore.html#tymethod.try_fill_bytes
     OsRng.try_fill_bytes(&mut key).unwrap();
+    println!("key is {:?}", key);
 
-    println!("{:?}", key);
+    // https://docs.rs/aes/latest/aes/#examples
+    let plaintext = GenericArray::from([0u8; 16]);
+    println!("plaintext is {:?}", plaintext);
+
+    let mut block = plaintext.clone();
+
+    let cipher = Aes128::new(&GenericArray::from(key));
+
+    // https://docs.rs/cipher/0.4.4/cipher/trait.BlockEncrypt.html#method.encrypt_block.
+    // Encrypt a single block in place.
+    cipher.encrypt_block(&mut block);
+    println!("ciphertext is {:?}", block);
+
+    cipher.decrypt_block(&mut block);
+    assert_eq!(plaintext, block);
+    println!("decrypted ciphertext is {:?}", block);
 }
