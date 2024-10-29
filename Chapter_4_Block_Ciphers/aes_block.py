50bc19d291cf8bda7e3cd70bb01407fb0dfcd976
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-29 23:24:59 +0000

    Remove hexlify dependency in favor of bytes.hex call.

--- Chapter_4_Block_Ciphers/aes_block.py
+++ Chapter_4_Block_Ciphers/aes_block.py
@@ -1,10 +1,9 @@
-from binascii import hexlify as hexa
-from os import urandom
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from os import urandom
 
 # Pick a random 16-byte key using Python's crypto PRNG.
 k = urandom(16)
-print(f"k = {hexa(k)}")
+print(f"k = {k.hex()}")
 
 # Create AES-128 block cipher to encrypt a single block.
 cipher = Cipher(algorithms.AES128(k), modes.ECB())
@@ -15,9 +14,9 @@ p = b'\x00'*16
 
 # Encrypt plaintext p (to ciphertext c).
 c = aes_encrypt.update(p) + aes_encrypt.finalize()
-print(f"enc({hexa(p)}) = {hexa(c)}")
+print(f"enc({p.hex()}) = {c.hex()}")
 
 # Decrypt ciphertext c to plaintext p.
 aes_decrypt = cipher.decryptor()
 p = aes_decrypt.update(c) + aes_decrypt.finalize()
-print(f"dec({hexa(c)}) = {hexa(p)}")
+print(f"dec({c.hex()}) = {p.hex()}")
