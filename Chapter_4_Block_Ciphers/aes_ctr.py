dfd23ae9c4a1a6f607ca52e63decd550bce0050c
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-30 23:23:55 +0000

    Try AES in CTR mode with PyCryptodome API.
    Remove binascii hexlify dependency.

--- Chapter_4_Block_Ciphers/aes_ctr.py
+++ Chapter_4_Block_Ciphers/aes_ctr.py
@@ -1,11 +1,10 @@
-from binascii import hexlify as hexa
 from os import urandom
 from struct import unpack
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 
 k = urandom(16)
-print(f"k = {hexa(k)}")
+print(f"k = {k.hex()}")
 
 # Pick a starting value for counter.
 nonce = unpack('<Q', urandom(8))[0]
@@ -20,10 +19,10 @@ p = b'\x00\x01\x02\x03'
 
 # Encrypt plaintext.
 c = aes.encrypt(p)
-print(f"enc({hexa(p)}) = {hexa(c)}")
+print(f"enc({p.hex()}) = {c.hex()}")
 
 # Decrypt ciphertext (using the encrypt function).
 ctr = Counter.new(128, initial_value=nonce)
 aes = AES.new(k, AES.MODE_CTR, counter=ctr)
 p = aes.encrypt(c)
-print(f"enc({hexa(c)}) = {hexa(p)}")
+print(f"enc({c.hex()}) = {p.hex()}")
